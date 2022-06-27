66668de87b6d570c0ea0947890d9da5743121b04
Thiago Costa <thiagonsc.costa@hotmail.com>
2022-06-26 23:54:49 -0300

    init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Body, OnModuleInit, Post, Put } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { AuthServiceClient, AUTH_SERVICE_NAME, LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from './auth.pb';
+
+@Injectable()
+export class AuthService implements OnModuleInit {
+
+    private svc: AuthServiceClient;
+
+    @Inject(AUTH_SERVICE_NAME)
+    private readonly client: ClientGrpc;
+
+    public onModuleInit(): void {
+        this.svc = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+    }
+
+    @Post('register')
+    private async register(@Body() body: RegisterRequest): Promise<Observable<RegisterResponse>> {
+        return this.svc.register(body);
+    }
+
+    @Put('login')
+    private async login(@Body() body: LoginRequest): Promise<Observable<LoginResponse>> {
+        return this.svc.login(body);
+    }
+}
---
+++ src/order/order.controller.ts
@@ -0,0 +1,29 @@
+import { Inject, OnModuleInit, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { CreateOrderRequest, CreateOrderResponse, OrderServiceClient, ORDER_SERVICE_NAME } from './order.pb';
+import { Request } from 'express';
+
+@Controller('order')
+export class OrderController implements OnModuleInit {
+    
+    private svc: OrderServiceClient;
+
+    @Inject(ORDER_SERVICE_NAME)
+    private readonly client: ClientGrpc;
+
+    onModuleInit(): void {
+        this.svc = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME);
+    }
+
+    @Post()
+    private async createOrder(@Req() req: Request): Promise<Observable<CreateOrderResponse>> {
+        const body: CreateOrderRequest = req.body;
+
+        body.userId = <number>req.user;
+
+        return this.svc.createOrder(body);
+    }
+
+}
